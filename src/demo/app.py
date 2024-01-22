e783d932df502b9bcf16523a8ba9107e2150124f
Evgeny Kungurov <johnk2280@gmail.com>
2024-01-22 21:22:45 +0500

    [upd] app.py

--- src/demo/app.py
+++ src/demo/app.py
@@ -1,3 +1,4 @@
+from fastapi import Body
 from fastapi import FastAPI
 from fastapi import Response
 
@@ -18,4 +19,9 @@ def greet_1(name: str):
 # Передача через query params в query string
 @app.get('/greet')
 def greet_2(name: str = 'World'):
+    return Response(f'Hello {name}')
+
+
+@app.post('/greet')
+def greet_3(name: str = Body(...)):
     return Response(f'Hello {name}')
