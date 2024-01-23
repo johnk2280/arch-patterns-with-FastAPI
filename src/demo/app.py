dd4282e7259683e0c7f30fef0e0bb5b57d2533e0
Evgeny Kungurov <johnk2280@gmail.com>
2024-01-23 10:35:18 +0500

    [upd] app.py - added get_accounts

--- src/demo/app.py
+++ src/demo/app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from fastapi import Form
+from fastapi import HTTPException
 from fastapi import Response
 from fastapi import status
 from passlib.hash import pbkdf2_sha256
+from sqlalchemy.exc import IntegrityError
 
 from demo.database import Account
 from demo.database import Session
@@ -23,6 +25,18 @@ def create_account(
             password=pbkdf2_sha256.hash(password),
         )
         session.add(account)
-        session.commit()
+
+        try:
+            session.commit()
+        except IntegrityError:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT)
 
     return Response('CREATED', status_code=status.HTTP_201_CREATED)
+
+
+@app.get('/accounts')
+def get_accounts():
+    with Session() as session:
+        accounts = session.query(Account).all()
+
+    return accounts
