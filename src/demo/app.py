5d497a1c4f28315e562f10f299dd19a5775ffae9
Evgeny Kungurov <johnk2280@gmail.com>
2024-01-23 10:30:01 +0500

    [upd] app.py - updated create_account

--- src/demo/app.py
+++ src/demo/app.py
@@ -1,18 +1,28 @@
-import json
-
-from fastapi import Body
 from fastapi import FastAPI
 from fastapi import Form
-from fastapi import Request
 from fastapi import Response
+from fastapi import status
+from passlib.hash import pbkdf2_sha256
 
-from demo.schemas import GreetingSchema
+from demo.database import Account
+from demo.database import Session
 
 app = FastAPI()
 
 
 @app.post('/account')
-def create_account(request: Request):
-    ...
+def create_account(
+    email: str = Form(...),
+    username: str = Form(...),
+    password: str = Form(),
+):
+    with Session() as session:
+        account = Account(
+            email=email,
+            username=username,
+            password=pbkdf2_sha256.hash(password),
+        )
+        session.add(account)
+        session.commit()
 
-
+    return Response('CREATED', status_code=status.HTTP_201_CREATED)
