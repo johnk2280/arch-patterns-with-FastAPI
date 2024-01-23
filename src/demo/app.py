522bfe637a5bf9400b55179d588ef1054b2affe4
Evgeny Kungurov <johnk2280@gmail.com>
2024-01-23 09:54:19 +0500

    [upd] app.py - added new handler for creating account

--- src/demo/app.py
+++ src/demo/app.py
@@ -11,39 +11,8 @@ from demo.schemas import GreetingSchema
 app = FastAPI()
 
 
-@app.get('/')
-def root():
-    return 'Hello World'
-
-
-# Передача через url
-@app.get('/greet/{name}')
-def greet_1(name: str):
-    return Response(f'Hello {name}')
-
-
-# Передача через query params в query string
-@app.get('/greet')
-def greet_2(name: str = 'World'):
-    return Response(f'Hello {name}')
-
-
-# Доступ к объекту Request
-@app.post('/greet_0')
-def greet_3(request: Request, name: str = Body(...), ):
-    return Response(f'Hello {name}. Request: {request}')
-
-
-# Десериализация (и очевидно валидация) входных данных,
-# поступающих в теле запроса в качестве JSON.
-@app.post('/greet_1')
-def greet_3(schema: GreetingSchema):
-    return Response(f'Hello {schema.name}')
-
-
-# Работа с формами: form-data или x-www-form-urlencoded
-@app.post('/greet')
-def greet_3(name: str = Form(...)):
-    return Response(f'Hello {name}')
+@app.post('/account')
+def create_account(request: Request):
+    ...
 
 
