aec28f4ea8e1342ff1f58608cb7883f9e7a4967b
Evgeny Kungurov <johnk2280@gmail.com>
2024-01-23 09:48:07 +0500

    [upd] database.sqlite

--- src/demo/database.py
+++ src/demo/database.py
@@ -37,4 +37,8 @@ class Account(Base):
     email = Column(String, nullable=False)
     username = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
+    first_name = Column(String)
+    last_name = Column(String)
+    avatar = Column(String)
+
 
