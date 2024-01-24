94dcccec7094b1b0973cc0aed0d7ca3c62e87de5
Evgeny Kungurov <johnk2280@gmail.com>
2024-01-24 14:52:41 +0500

    [add] test database

---
+++ tests/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Engine
+from sqlalchemy import event
+from sqlalchemy.orm import sessionmaker
+
+from demo.config import settings
+
+
+# Функция обработчик для автоматического создания внешних ключей в БД SQLite.
+# Только для SQLite
+
+
+@event.listens_for(Engine, 'connect')
+def enable_foreign_keys(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute('PRAGMA foreign_keys=ON')
+    cursor.close()
+
+
+engine = create_engine(
+    settings.TEST_DATABASE_URL,
+    future=True,  # только для SQLite
+    connect_args={'check_same_thread': False},  # только для SQLite
+)
+
+
+Session = sessionmaker(engine, future=True)
---
+++ tests/demo/test_app.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+
+from demo.app import app
+
+client = TestClient(app)
+
+
+def test_get_account():
+    # arrange
+    url = '/accounts/1'
+
+    # act
+    response = client.get(url)
+
+    # assert
+    assert response.status_code == 200
+    assert response.json()['id'] == 1
