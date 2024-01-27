8207c660dedf8570305e4eb5501b9f7bb6f5de29
Evgeny Kungurov <johnk2280@gmail.com>
2024-01-27 15:55:14 +0500

    [add] orm.py

---
+++ src/ch_02/orm.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import MetaData
+from sqlalchemy import String
+from sqlalchemy import Table
+from sqlalchemy.orm import Mapper
+from sqlalchemy.orm import registry
+
+from ch_02.model import OrderLine
+
+metadata = MetaData()
+mapper_registry = registry()
+
+order_lines = Table(
+    'order_lines',
+    mapper_registry.metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('sku', String(255)),
+    Column('qty', Integer, nullable=False),
+    Column('order_id', String(255)),
+)
+
+
+def start_mappers() -> None:
+    """
+    see more about 'classical mapping':
+    https://docs.sqlalchemy.org/en/20/orm/mapping_styles.html#classical-mappings
+    """
+    lines_mapper: Mapper = mapper_registry.map_imperatively(OrderLine, order_lines)
+    print(lines_mapper)
+
+
+if __name__ == '__main__':
+    start_mappers()
