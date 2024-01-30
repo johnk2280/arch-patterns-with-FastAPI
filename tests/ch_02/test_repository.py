899eb5a60b7ab469db6eeee08e362ac1e9df266a
Evgeny Kungurov <johnk2280@gmail.com>
2024-01-30 08:59:47 +0500

    [add] insert_order_line for tests

--- tests/ch_02/test_repository.py
+++ tests/ch_02/test_repository.py
@@ -1,5 +1,8 @@
+from sqlalchemy import insert
+from sqlalchemy import text
 from sqlalchemy.orm import Session
 
+from ch_02.model import OrderLine
 from ch_02.repository import SQLAlchemyRepository
 
 
@@ -14,3 +17,19 @@ def test_repository_can_save_a_batch(session: Session):
 
     assert len(rows) == 1
     assert rows == [batch]
+
+
+def insert_order_line(session: Session) -> str:
+    session.execute(
+        text(
+            'INSERT INTO order_lines (orderid, sku, qty)'
+            ' VALUES ("order1", "GENERIC-SOFA", 12)',
+        ),
+    )
+    [[order_line_id]] = session.execute(
+        text(
+            "SELECT id FROM order_lines WHERE orderid=:orderid AND sku=:sku",
+        ),
+        dict(orderid="order1", sku="GENERIC-SOFA"),
+    )
+    return order_line_id
