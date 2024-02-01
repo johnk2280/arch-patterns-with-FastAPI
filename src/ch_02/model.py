a0919551cc289e4deb7c2b00f2185bb783c10922
Evgeny Kungurov <johnk2280@gmail.com>
2024-02-01 08:35:59 +0500

    [upd] AbstractRepository, SQLAlchemyRepository

--- src/ch_02/model.py
+++ src/ch_02/model.py
@@ -15,7 +15,6 @@ class OrderLine:
 
 
 class Batch:
-
     def __init__(
         self,
         ref: str,
--- src/ch_02/repository.py
+++ src/ch_02/repository.py
@@ -1,22 +1,30 @@
 from abc import ABC
 from abc import abstractmethod
+from typing import Generic
+from typing import TypeVar
 
 from sqlalchemy.orm import Session
 
 from ch_02.model import Batch
 
+T = TypeVar('T')
 
-class AbstractRepository(ABC):
+
+class AbstractRepository(ABC, Generic[T]):
     @abstractmethod
-    def add(self, batch: Batch) -> None:
+    def add(self, item: T) -> None:
         pass
 
     @abstractmethod
-    def get(self, reference) -> Batch:
+    def get(self, reference: str) -> T:
+        pass
+
+    @abstractmethod
+    def list(self) -> list[T]:
         pass
 
 
-class SQLAlchemyRepository(AbstractRepository):
+class SQLAlchemyRepository(AbstractRepository[Batch]):
     def __init__(self, session: Session) -> None:
         self.session = session
 
@@ -25,3 +33,6 @@ class SQLAlchemyRepository(AbstractRepository):
 
     def get(self, reference: str) -> Batch:
         return self.session.query(Batch).filter_by(reference=reference).one()
+
+    def list(self) -> list[Batch]:
+        return self.session.query(Batch).all()
