424a163f747285163d233c0ec0bd39e9e7c07d54
Evgeny Kungurov <johnk2280@gmail.com>
2024-02-02 10:30:29 +0500

    [upd] sync

--- src/ch_03/utils.py
+++ src/ch_03/utils.py
@@ -17,38 +17,72 @@ def hash_file(path: Path) -> str:
     return hasher.hexdigest()
 
 
+# TODO: реализовать
+def read_path_and_hashes(dest: str) -> dict[str, str]:
+    pass
+
+
+# TODO: реализовать
+def determine_actions(
+    source_hashes: dict[str, str],
+    dest_hashes: dict[str, str],
+    source: str,
+    dest: str,
+) -> list[tuple[str, str, str]]:
+    pass
+
+
 def sync(source: str, dest: str) -> None:
-    # Обойти исходную папку и создать словарь имен файлов
+    # Шаг 1 с императивным ядром: собрать входные данные
+    source_hashes = read_path_and_hashes(source)
+    dest_hashes = read_path_and_hashes(dest)
 
-    source_hashes = {}
-    for folder, _, files in os.walk(source):
-        for file in files:
-            source_hashes[hash_file(Path(folder) / file)] = file
+    # Шаг 2: Вызвать функциональное ядро
+    actions = determine_actions(source_hashes, dest_hashes, source, dest)
 
-    seen = set()  # Отслеживать фалы, найденные в целевой папке
+    # Шаг 3 С императивным ядром: применить операции ввода-вывода данных
+    for action, *paths in actions:
+        match action.upper():
+            case 'COPY':
+                shutil.copyfile(*paths)
+            case 'MOVE':
+                shutil.move(*paths)
+            case 'DELETE':
+                os.unlink(paths[0])
+            case _:
+                raise ValueError(f'Unknown action: {action}')
 
-    # Обойти целевую папку и получить имена файлов и хеши
-    for folder, _, files in os.walk(dest):
-        for file in files:
-            dest_path = Path(folder) / file
-            dest_hash = hash_file(dest_path)
-            seen.add(dest_hash)
-
-            # Если в целевой папке есть файл,
-            # которого нет в источнике, то удалить
-            if dest_hash not in source_hashes:
-                dest_path.unlink()
-
-            # если в целевой папке есть файл, который имеет другой путь
-            # в источнике, то переместить его в правильный путь
-            elif (dest_hash in source_hashes
-                  and file != source_hashes[dest_hash]):
-                shutil.move(dest_path, Path(folder) / source_hashes[dest_hash])
-
-    # Каждый файл, который появляется в источнике, но не в месте назначения,
-    # скопировать в целевую папку
-    for src_hash, file in source_hashes.items():
-        if src_hash not in seen:
-            shutil.copy(Path(source) / file, Path(dest) / file)
-
-
+# def sync(source: str, dest: str) -> None:
+#     # Обойти исходную папку и создать словарь имен файлов
+#
+#     source_hashes = {}
+#     for folder, _, files in os.walk(source):
+#         for file in files:
+#             source_hashes[hash_file(Path(folder) / file)] = file
+#
+#     seen = set()  # Отслеживать фалы, найденные в целевой папке
+#
+#     # Обойти целевую папку и получить имена файлов и хеши
+#     for folder, _, files in os.walk(dest):
+#         for file in files:
+#             dest_path = Path(folder) / file
+#             dest_hash = hash_file(dest_path)
+#             seen.add(dest_hash)
+#
+#             # Если в целевой папке есть файл,
+#             # которого нет в источнике, то удалить
+#             if dest_hash not in source_hashes:
+#                 dest_path.unlink()
+#
+#             # если в целевой папке есть файл, который имеет другой путь
+#             # в источнике, то переместить его в правильный путь
+#             elif (dest_hash in source_hashes
+#                   and file != source_hashes[dest_hash]):
+#                 shutil.move(dest_path, Path(folder) / source_hashes[
+#                 dest_hash])
+#
+#     # Каждый файл, который появляется в источнике, но не в месте назначения,
+#     # скопировать в целевую папку
+#     for src_hash, file in source_hashes.items():
+#         if src_hash not in seen:
+#             shutil.copy(Path(source) / file, Path(dest) / file)
