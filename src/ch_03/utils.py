f036cfef83e50615590e1f3384739239d2f7ad47
Evgeny Kungurov <johnk2280@gmail.com>
2024-02-03 09:05:51 +0500

    [upd] utils.py - added protocols

--- src/ch_03/utils.py
+++ src/ch_03/utils.py
@@ -1,8 +1,10 @@
 import hashlib
 import os
 import shutil
+from collections.abc import Callable
 from collections.abc import Generator
 from pathlib import Path
+from typing import Protocol
 
 BLOCK_SIZE = 65536
 
@@ -92,27 +94,81 @@ def determine_actions(
             yield 'DELETE', dest / filename, ''
 
 
-def sync(source: str, dest: str) -> None:
+# def sync(source: str, dest: str) -> None:
+#     # Шаг 1 с императивным ядром: собрать входные данные
+#     source_hashes = read_path_and_hashes(source)
+#     dest_hashes = read_path_and_hashes(dest)
+#
+#     # Шаг 2: Вызвать функциональное ядро
+#     actions = determine_actions(
+#         source_hashes,
+#         dest_hashes,
+#         Path(source),
+#         Path(dest),
+#     )
+#
+#     # Шаг 3 С императивным ядром: применить операции ввода-вывода данных
+#     for action, *paths in actions:
+#         match action.upper():
+#             case 'COPY':
+#                 shutil.copyfile(*paths)
+#             case 'MOVE':
+#                 shutil.move(*paths)
+#             case 'DELETE':
+#                 os.unlink(paths[0])
+#             case _:
+#                 raise ValueError(f'Unknown action: {action}')
+
+
+# Третья итерация. Внедряем новые зависимости в верхнеуровневую функцию sync().
+# Теперь sync() принимает на вход (выставляет наружу) 2 новые зависимости:
+# filesystem и reader.
+
+class HasCopy(Protocol):
+    def copy(self, src: Path, dest: Path) -> None: ...
+
+
+class HasMove(Protocol):
+    def move(self, src: Path, dest: Path) -> None: ...
+
+
+class HasDelete(Protocol):
+    def delete(self, src: Path, dest: Path) -> None: ...
+
+
+class FileSystemProtocol(HasCopy, HasMove, HasDelete, Protocol):
+    ...
+
+
+class FakeFileSystem(FileSystemProtocol): ...
+
+def sync(
+    reader: Callable[[str], dict[str, str]],
+    filesystem: FileSystemProtocol,
+    source: str,
+    dest: str
+) -> None:
     # Шаг 1 с императивным ядром: собрать входные данные
-    source_hashes = read_path_and_hashes(source)
-    dest_hashes = read_path_and_hashes(dest)
+    source_hashes = reader(source)
+    dest_hashes = reader(dest)
 
-    # Шаг 2: Вызвать функциональное ядро
-    actions = determine_actions(
-        source_hashes,
-        dest_hashes,
-        Path(source),
-        Path(dest),
-    )
+    # Шаг 2: Вызвать функциональное ядро. Функциональное перенесено обратно
+    # из determine_actions() в верхнеуровневую функцию sync().
+    for sha, filename in source_hashes.items():
+        # Если в целевой папке отсутствует фал
+        if sha not in dest_hashes:
+            source_path = Path(source) / filename
+            dest_path = Path(dest) / filename
+            yield 'COPY', source_path, dest_path
 
-    # Шаг 3 С императивным ядром: применить операции ввода-вывода данных
-    for action, *paths in actions:
-        match action.upper():
-            case 'COPY':
-                shutil.copyfile(*paths)
-            case 'MOVE':
-                shutil.move(*paths)
-            case 'DELETE':
-                os.unlink(paths[0])
-            case _:
-                raise ValueError(f'Unknown action: {action}')
+        # Если в целевой папке имеется файл, который имеет другой путь
+        elif dest_hashes[sha] != filename:
+            old_dest_path = Path(dest) / dest_hashes[sha]
+            new_dest_path = Path(dest) / filename
+            yield 'MOVE', old_dest_path, new_dest_path
+
+    # Если в целевой папке имеется файл, который отсутствует в источнике
+    for sha, filename in dest_hashes.items():
+        if sha not in source_hashes:
+            yield 'DELETE', dest / filename, ''
+
