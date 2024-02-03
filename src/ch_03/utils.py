d19d03413bf80a3b7258667caf8e42f1a13775d6
Evgeny Kungurov <johnk2280@gmail.com>
2024-02-03 09:29:40 +0500

    [upd] test_when_a_file_exists_int_the_source_but_not_in_destination

--- src/ch_03/utils.py
+++ src/ch_03/utils.py
@@ -1,6 +1,5 @@
 import hashlib
 import os
-import shutil
 from collections.abc import Callable
 from collections.abc import Generator
 from pathlib import Path
@@ -157,6 +156,10 @@ class FakeFileSystem(FileSystemProtocol):
     def delete(self, dest: Path) -> None:
         self._commands.append(('DELETE', dest, Path('')))
 
+    @property
+    def commands(self) -> list[tuple[CommandTypes, Path, Path]]:
+        return self._commands
+
 
 def sync(
     reader: Callable[[str], dict[str, str]],
@@ -175,15 +178,15 @@ def sync(
         if sha not in dest_hashes:
             source_path = Path(source) / filename
             dest_path = Path(dest) / filename
-            yield 'COPY', source_path, dest_path
+            filesystem.copy(source_path, dest_path)
 
         # Если в целевой папке имеется файл, который имеет другой путь
         elif dest_hashes[sha] != filename:
             old_dest_path = Path(dest) / dest_hashes[sha]
             new_dest_path = Path(dest) / filename
-            yield 'MOVE', old_dest_path, new_dest_path
+            filesystem.move(old_dest_path, new_dest_path)
 
     # Если в целевой папке имеется файл, который отсутствует в источнике
     for sha, filename in dest_hashes.items():
         if sha not in source_hashes:
-            yield 'DELETE', dest / filename, ''
+            filesystem.delete(Path(dest) / filename)
--- tests/ch_02/test_utils.py
+++ tests/ch_02/test_utils.py
@@ -1,21 +1,24 @@
-import shutil
-import tempfile
 from pathlib import Path
 
 from ch_03.utils import determine_actions
+from ch_03.utils import FakeFileSystem
+from ch_03.utils import read_path_and_hashes
 from ch_03.utils import sync
 
 
 def test_when_a_file_exists_int_the_source_but_not_in_destination():
     src_hashes = {'hash1': 'fn1'}
     dst_hashes = {}
-    actions = determine_actions(
-        src_hashes,
-        dst_hashes,
-        Path('/src'),
-        Path('/dest'),
-    )
-    assert list(actions) == [('COPY', Path('/src/fn1'), Path('/dest/fn1'))]
+
+    filesystem = FakeFileSystem()
+
+    # fake reader
+    reader = {'/src': src_hashes, '/dest': dst_hashes}
+
+    sync(reader.pop, filesystem, '/src', '/dest')
+    assert filesystem.commands == [
+        ('COPY', Path('/src/fn1'), Path('/dest/fn1')),
+    ]
 
 
 def test_when_a_file_has_been_renamed__in_the_source():
