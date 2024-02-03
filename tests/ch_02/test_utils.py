cdaf7d9f1fa630473f4ac9f369a99073d8a8a687
Evgeny Kungurov <johnk2280@gmail.com>
2024-02-03 08:34:12 +0500

    [upd] test_when_a_file_exists_int_the_source_but_not_in_destination()

--- tests/ch_02/test_utils.py
+++ tests/ch_02/test_utils.py
@@ -2,23 +2,20 @@ import shutil
 import tempfile
 from pathlib import Path
 
+from ch_03.utils import determine_actions
 from ch_03.utils import sync
 
 
 def test_when_a_file_exists_int_the_source_but_not_in_destination():
-    try:
-        source = tempfile.mkdtemp()
-        dest = tempfile.mkdtemp()
-        content = 'Я тестовый файл!'
-        (Path(source) / 'my-file').write_text(content)
-        sync(source, dest)
-
-        expected_path = Path(dest) / 'my-file'
-        assert expected_path.exists()
-        assert expected_path.read_text() == content
-    finally:
-        shutil.rmtree(source)
-        shutil.rmtree(dest)
+    src_hashes = {'hash1': 'fn1'}
+    dst_hashes = {}
+    actions = determine_actions(
+        src_hashes,
+        dst_hashes,
+        Path('/src'),
+        Path('/dest'),
+    )
+    assert list(actions) == [('COPY', Path('/src/fn1'), Path('/dest/fn1'))]
 
 
 def test_when_a_file_has_been_renamed__in_the_source():
