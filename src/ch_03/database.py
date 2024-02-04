2438b6ab6b6fd71099b780fe70e4e2f4ad45167d
Evgeny Kungurov <johnk2280@gmail.com>
2024-02-04 09:12:20 +0500

    [add] database.py for initialization AsyncSession

---
+++ src/ch_03/database.py
@@ -0,0 +1,25 @@
+from collections.abc import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
+
+from config import settings
+
+engine = create_async_engine(
+    settings.database.url,
+    # future=True,
+    # pool_pre_ping=True,
+    # echo=False,
+)
+
+Session = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession,
+)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with Session() as session:
+        yield session
