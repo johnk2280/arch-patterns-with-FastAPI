3c8b105f926a65e74714c713662548badcfccae4
Evgeny Kungurov <johnk2280@gmail.com>
2024-02-07 21:16:39 +0500

    [add] FastAPI app factory

---
+++ src/entrypoints/fastapi_app.py
@@ -0,0 +1,17 @@
+from collections.abc import Iterable
+
+from fastapi import APIRouter
+from fastapi import FastAPI
+
+
+def create(routers: Iterable[APIRouter]) -> FastAPI:
+    """FastAPI application factory."""
+    application = FastAPI()
+
+    for router in routers:
+        application.include_router(router)
+
+    return application
+
+
+app = create([APIRouter()])
