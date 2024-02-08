306b944641ca4d4bdbd32a59dbb613148e020e31
Evgeny Kungurov <johnk2280@gmail.com>
2024-02-08 09:54:02 +0500

    [upd] orm's entities

--- src/infrastructure/rest_api/views.py
+++ src/infrastructure/rest_api/views.py
@@ -4,14 +4,13 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from domain.model import Batch
-from infrastructure.orm.database import get_async_session
+from infrastructure.adapters.orm import get_async_session
 
 router = APIRouter(prefix='', tags=['batches'])
 
 
 @router.get('/batches')
-async def get_batches():
-    session = next(get_async_session())
+async def get_batches(session: AsyncSession = Depends(get_async_session)):
     stmt = select(Batch)
     result = await session.execute(stmt)
     return result.mappings().all()
