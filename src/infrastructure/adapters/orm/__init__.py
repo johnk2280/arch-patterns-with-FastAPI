4c31681684c9c014d1706cd17a2c1a46e52a1ccf
Evgeny Kungurov <johnk2280@gmail.com>
2024-02-21 21:21:13 +0500

    [upd] get_batches() handler

--- src/infrastructure/adapters/orm/__init__.py
+++ src/infrastructure/adapters/orm/__init__.py
@@ -1,8 +1,14 @@
 from .database import get_async_session
+from .orm import allocations
+from .orm import batches
 from .orm import mapper_registry
+from .orm import order_lines
 
 __all__ = [
     'get_async_session',
     'mapper_registry',
+    'order_lines',
+    'batches',
+    'allocations',
 
 ]
--- src/infrastructure/rest_api/views.py
+++ src/infrastructure/rest_api/views.py
@@ -3,8 +3,7 @@ from fastapi import Depends
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-# from domain.model import Batch
-from domain.model import Batch
+from infrastructure.adapters.orm import batches
 from infrastructure.adapters.orm import get_async_session
 
 router = APIRouter(prefix='', tags=['batches'])
@@ -14,10 +13,7 @@ router = APIRouter(prefix='', tags=['batches'])
 async def get_batches(
     session: AsyncSession = Depends(get_async_session),
 ):
-    # TODO: sqlalchemy.exc.ArgumentError:
-    #  Column expression, FROM clause, or other columns clause element
-    #  expected, got <class 'domain.model.Batch'>.
-    stmt = select(Batch)
+    stmt = select(batches)
     result = await session.execute(stmt)
     return result.mappings().all()
 
