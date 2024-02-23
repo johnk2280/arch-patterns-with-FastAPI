9d8a934e818847fc75eadad86bdfc5acd4d0096a
Evgeny Kungurov <johnk2280@gmail.com>
2024-02-23 11:41:04 +0500

    [add] AbstractAsyncRepository

--- src/infrastructure/adapters/repositories/repository.py
+++ src/infrastructure/adapters/repositories/repository.py
@@ -1,27 +1,28 @@
 from typing import Any
 
 from sqlalchemy import select
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from domain.model import Batch
 from service_layer.ports import AbstractRepository
 
 
 # TODO: Доработать репозиторий
-class BatchRepository(AbstractRepository[Batch, Session]):
+class BatchRepository(AbstractRepository[Batch, AsyncSession]):
 
     model_class = Batch
 
-    def __init__(self, session: Session) -> None:
+    def __init__(self, session: AsyncSession) -> None:
         super().__init__(session)
 
     def add(self, batch: Batch) -> None:
         self.session.add(batch)
 
-    def get(self, reference: str) -> Batch:
+    async def get(self, reference: str) -> Batch:
         stmt = select(Batch).filter_by(reference=reference)
         # return self.session.query(Batch).filter_by(reference=reference).one()
-        return self.session.execute(stmt).scalar()
+        result = await self.session.execute(stmt)
+        return result.scalar()
 
     def list(self) -> list[Batch]:
         stmt = select(Batch)
--- src/service_layer/ports/repository.py
+++ src/service_layer/ports/repository.py
@@ -3,8 +3,12 @@ from abc import abstractmethod
 from typing import Generic
 from typing import TypeVar
 
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session
+
 M = TypeVar('M')  # subtype of DeclarativeBase
-S = TypeVar('S')  # any session
+S = TypeVar('S', bound=Session)
+AS = TypeVar('AS', bound=AsyncSession)
 
 
 class AbstractRepository(ABC, Generic[M, S]):
@@ -29,4 +33,29 @@ class AbstractRepository(ABC, Generic[M, S]):
 
     @abstractmethod
     def list(self) -> list[M]:
+        pass
+
+
+class AbstractAsyncRepository(ABC, Generic[M, AS]):
+
+    model_class: type[M]
+
+    def __init__(self, session: AS) -> None:
+        if not self.model_class:
+            raise ValueError(
+                'Can not initiate the class without model_class attribute',
+            )
+
+        self.session = session
+
+    @abstractmethod
+    async def aadd(self, item: M) -> None:
+        pass
+
+    @abstractmethod
+    async def aget(self, reference: str) -> M:
+        pass
+
+    @abstractmethod
+    async def alist(self) -> list[M]:
         pass
