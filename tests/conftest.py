26f97c4b9ab42ae79a0a1c942319e4376e350238
Evgeny Kungurov <johnk2280@gmail.com>
2024-02-25 08:51:52 +0500

    [upd] added simple async function and test for it for chekc pytest_asyncio setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,12 @@
+from collections.abc import AsyncGenerator
+
 import pytest
+import pytest_asyncio
 from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import clear_mappers
 from sqlalchemy.orm import sessionmaker
 
@@ -15,7 +22,29 @@ def in_memory_db():
 
 
 @pytest.fixture
+def async_in_memory_db():
+    engine = create_async_engine('sqlite:///:memory:')
+    mapper_registry.metadata.create_all(engine)
+    return engine
+
+
+@pytest.fixture
 def session(in_memory_db):
     start_mappers()
     yield sessionmaker(bind=in_memory_db, future=True)()
     clear_mappers()
+
+
+@pytest_asyncio.fixture
+async def async_session(
+    async_in_memory_db: AsyncEngine,
+) -> AsyncGenerator[AsyncSession, None]:
+    start_mappers()
+    async_session = async_sessionmaker(
+        bind=async_in_memory_db,
+        expire_on_commit=False,
+        class_=AsyncSession,
+    )
+    async with async_session() as session:
+        yield session
+        clear_mappers()
--- tests/integration/test_async_batch_repository.py
+++ tests/integration/test_async_batch_repository.py
@@ -8,16 +8,27 @@ from domain.model import Batch
 from infrastructure.adapters.repositories import AsyncBatchRepository
 
 
-# TODO: Настроить асинхронные тесты
+# Для проверки подключения pytest_asyncio
+async def my_coroutine() -> float:
+    await asyncio.sleep(0.1)
+    return 2.71828182845
+
 
 @pytest.mark.asyncio
-async def test_async_batch_repository_can_save_a_batch(session: AsyncSession):
+async def test_my_coroutine() -> None:
+    assert 2 < await my_coroutine() < 3
+
+
+@pytest.mark.asyncio
+async def test_async_batch_repository_can_save_a_batch(
+    async_session: AsyncSession,
+):
     batch = Batch('batch-1', 'RUSTY-SOAPDISH', 100)
-    repo = AsyncBatchRepository(session)
+    repo = AsyncBatchRepository(async_session)
     await repo.aadd(batch)
 
     stmt = select(Batch)
-    rows = await session.execute(stmt)
+    rows = await async_session.execute(stmt)
 
     assert len(rows.scalars()) == 1
     assert rows == [batch]
