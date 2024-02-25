8fbe0067e5c4828fb06409e2db1c38c060deb5f4
Evgeny Kungurov <johnk2280@gmail.com>
2024-02-25 10:36:00 +0500

    [upd] test_repository

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ import pytest
 import pytest_asyncio
 from sqlalchemy import create_engine
 from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncConnection
 from sqlalchemy.ext.asyncio import AsyncEngine
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
@@ -22,13 +23,6 @@ def in_memory_db():
 
 
 @pytest.fixture
-def async_in_memory_db():
-    engine = create_async_engine('sqlite+aiosqlite:///:memory:')
-    mapper_registry.metadata.create_all(engine)
-    return engine
-
-
-@pytest.fixture
 def session(in_memory_db):
     start_mappers()
     yield sessionmaker(bind=in_memory_db, future=True)()
@@ -36,15 +30,30 @@ def session(in_memory_db):
 
 
 @pytest_asyncio.fixture
+def async_engine() -> AsyncEngine:
+    engine = create_async_engine('sqlite+aiosqlite:///:memory:')
+    return engine
+
+
+@pytest_asyncio.fixture
+async def create(async_engine: AsyncEngine):
+    async with async_engine.begin() as conn:
+        conn.run_sync(mapper_registry.metadata.create_all)
+        yield conn
+        conn.run_sync(mapper_registry.metadata.drop_all)
+
+    # async with async_engine.begin() as conn:
+    #     conn.run_sync(mapper_registry.metadata.drop_all)
+    # mapper_registry.metadata.create_all(engine)
+    # return engine
+
+
+@pytest_asyncio.fixture
 async def async_session(
-    async_in_memory_db: AsyncEngine,
+    async_engine: AsyncEngine,
+    create: AsyncGenerator,
 ) -> AsyncGenerator[AsyncSession, None]:
-    start_mappers()
-    async_session = async_sessionmaker(
-        bind=async_in_memory_db,
-        expire_on_commit=False,
-        class_=AsyncSession,
-    )
-    async with async_session() as session:
+    # start_mappers()
+    async with AsyncSession(bind=async_engine) as session:
         yield session
-        clear_mappers()
+
--- tests/integration/test_repository.py
+++ tests/integration/test_repository.py
@@ -66,14 +66,14 @@ def insert_allocation(
     )
 
 
-async def test_repository_can_retrieve_a_batch_with_allocations(session: Session):
+def test_repository_can_retrieve_a_batch_with_allocations(session: Session):
     order_line_id = insert_order_line(session)
     batch_id_1 = insert_batch(session, 'batch-1')
     insert_batch(session, 'batch-2')
     insert_allocation(session, order_line_id, batch_id_1)
 
     repo = BatchRepository(session)
-    retrieved = await repo.get('batch-1')
+    retrieved = repo.get('batch-1')
 
     expected = Batch('batch-1', 'GENERIC-SOFA', 100)
 
