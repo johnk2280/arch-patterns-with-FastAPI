7cdbcf187f43b6cbdf6748c7b48631d9a2082007
Evgeny Kungurov <johnk2280@gmail.com>
2024-02-29 20:49:23 +0500

    [upd] test_async_batch_repository_can_retrieve_a_batch_with_allocation

--- tests/integration/test_batch_repository_async.py
+++ tests/integration/test_batch_repository_async.py
@@ -62,9 +62,6 @@ async def test_async_batch_repository_can_retrieve_a_batch_with_allocation(
             ],
         )
     )
-    print(batch)
-    print(batch.id)
-    print(order_line)
 
     repo = AsyncBatchRepository(async_session)
     retrieved = await repo.get('batch-1')
@@ -74,22 +71,6 @@ async def test_async_batch_repository_can_retrieve_a_batch_with_allocation(
     assert retrieved == expected
     assert retrieved.sku == expected.sku
     assert retrieved._purchased_quantity == expected._purchased_quantity
-    assert retrieved._allocations == expected._allocations
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    assert retrieved._allocations == {
+        OrderLine(order_id='order-1', sku='RED-CHAIR', qty=12, id=1),
+    }
