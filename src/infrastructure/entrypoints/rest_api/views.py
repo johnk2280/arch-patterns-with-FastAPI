85f3e8689fc85e61e020e6c85791e60cc32dbabb
Evgeny Kungurov <johnk2280@gmail.com>
2024-03-13 21:06:29 +0500

    [add] allocate view

--- src/infrastructure/entrypoints/rest_api/views.py
+++ src/infrastructure/entrypoints/rest_api/views.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 from fastapi import Depends
+from fastapi import Request
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from infrastructure.adapters.orm import batches
 from infrastructure.adapters.orm import get_async_session
+from infrastructure.adapters.repositories import AsyncBatchRepository
 
 router = APIRouter(prefix='', tags=['batches'])
 
@@ -17,3 +19,11 @@ async def get_batches(
     result = await session.execute(stmt)
     return result.mappings().all()
 
+
+@router.post('/allocate')
+async def allocate(
+    request: Request,
+    repo: AsyncBatchRepository = Depends(),
+):
+    batches = await repo.get_all()
+    line
