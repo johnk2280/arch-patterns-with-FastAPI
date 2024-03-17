787e45db55af606f5055223269c8e05a61f732c1
Evgeny Kungurov <johnk2280@gmail.com>
2024-03-17 12:07:33 +0500

    [upd] imports

--- src/infrastructure/adapters/repositories/sqla_repository.py
+++ src/infrastructure/adapters/repositories/sqla_repository.py
@@ -4,8 +4,8 @@ from typing import TypeVar
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from domain.models import Batch
-from domain.models import DomainModel
+from domain import Batch
+from domain import DomainModel
 from service_layer.ports import AbstractRepository
 
 M = TypeVar("M", bound=DomainModel)
--- tests/fake_repository.py
+++ tests/fake_repository.py
@@ -1,9 +1,9 @@
+from collections.abc import Iterable
 from typing import Any
 from typing import Generic
 from typing import TypeVar
 
-from domain.models import Batch
-from domain.models import DomainModel
+from domain import DomainModel
 from service_layer.ports import AbstractRepository
 
 T = TypeVar('T', bound=DomainModel)
@@ -26,11 +26,11 @@ class FakeRepository(AbstractRepository, Generic[T]):
     def _delete(self, *args, **kwargs) -> Any:
         raise NotImplementedError
 
-    def add(self, item: Batch) -> None:
+    def add(self, item: T) -> None:
         raise NotImplementedError
 
-    def get(self, reference: str) -> Batch:
+    def get(self, reference: str) -> T:
         raise NotImplementedError
 
-    def list(self) -> list[Batch]:
+    def list(self) -> Iterable[T]:
         raise NotImplementedError
