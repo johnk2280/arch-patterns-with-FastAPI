9a73d922ab03ea5a6e4d5b5cb4cb09e4129dfcdb
Evgeny Kungurov <johnk2280@gmail.com>
2024-03-23 10:31:55 +0500

    [upd] AbstractRepository

--- src/service_layer/ports/repository.py
+++ src/service_layer/ports/repository.py
@@ -1,13 +1,26 @@
 from abc import ABC
 from abc import abstractmethod
 from typing import Any
+from typing import Generic
+from typing import TypeVar
+
+T = TypeVar('T')
 
 
-class AbstractRepository(ABC):
+class AbstractRepository(ABC, Generic[T]):
+
+    def add(self, *args, **kwargs) -> Any:
+        raise NotImplementedError
 
     @abstractmethod
     def _create(self, *args, **kwargs) -> Any:
         pass
+
+    def get(self, *args, **kwargs) -> Any:
+        raise NotImplementedError
+
+    def get_many(self) -> Any:
+        raise NotImplementedError
 
     @abstractmethod
     def _read(self, *args, **kwargs) -> Any:
@@ -20,12 +33,3 @@ class AbstractRepository(ABC):
     @abstractmethod
     def _delete(self, *args, **kwargs) -> Any:
         pass
-
-    def get(self, *args, **kwargs) -> Any:
-        raise NotImplementedError
-
-    def add(self, *args, **kwargs) -> Any:
-        raise NotImplementedError
-
-    def list(self) -> Any:
-        raise NotImplementedError
