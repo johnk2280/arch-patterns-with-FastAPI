45a0b59998fb6f4d751fdd4680d208a1aac89eff
Evgeny Kungurov <johnk2280@gmail.com>
2024-04-04 09:25:22 +0500

    [upd] batch unit of work

--- src/service_layer/uow.py
+++ src/service_layer/uow.py
@@ -1,5 +1,17 @@
+from typing import Any
+
 from service_layer.ports import AbstractUOW
 
 
 class BatchUOW(AbstractUOW):
-    pass
+    async def __aenter__(self) -> Any:
+        pass
+
+    async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> Any:
+        pass
+
+    async def commit(self) -> None:
+        pass
+
+    async def rollback(self) -> None:
+        pass
