c0c6655b52e1ad381c66770e1a7ff3ffa1bf7229
Evgeny Kungurov <johnk2280@gmail.com>
2024-04-04 09:24:12 +0500

    [upd] abstract unit of work

--- src/service_layer/ports/abstract_unit_of_work.py
+++ src/service_layer/ports/abstract_unit_of_work.py
@@ -1,4 +1,6 @@
 from abc import ABC
+from abc import abstractmethod
+from typing import Any
 
 from domain import Batch
 from .abstract_repository import AbstractRepository
@@ -7,3 +9,19 @@ from .abstract_repository import AbstractRepository
 class AbstractUOW(ABC):
 
     batches: AbstractRepository[Batch]
+
+    @abstractmethod
+    async def __aenter__(self) -> Any:
+        pass
+
+    @abstractmethod
+    async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> Any:
+        pass
+
+    @abstractmethod
+    async def commit(self) -> None:
+        pass
+
+    @abstractmethod
+    async def rollback(self) -> None:
+        pass
