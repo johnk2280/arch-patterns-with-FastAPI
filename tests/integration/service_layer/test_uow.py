88a80d781ea4983364e06fdad536e6df5bf4926c
Evgeny Kungurov <johnk2280@gmail.com>
2024-04-10 20:15:37 +0500

    [add] test_uow_can_retrieve_a_batch_allocate_to_it

--- tests/integration/service_layer/test_uow.py
+++ tests/integration/service_layer/test_uow.py
@@ -1,7 +1,34 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from domain import Batch
+from domain import OrderLine
+from infrastructure.storage.orm import allocations
+from infrastructure.storage.repositories import AsyncBatchRepo
+from service_layer.uow import BatchUOW
 
 
 async def test_uow_can_retrieve_a_batch_allocate_to_it(
     async_session: AsyncSession,
 ):
-    pass
+    repo = AsyncBatchRepo(async_session)
+    await repo.add(
+            {
+                'reference': 'batch-1',
+                'sku': 'HIPSTER-WORKBENCH',
+                '_purchased_quantity': 100,
+                'eta': None,
+                },
+            )
+    await async_session.commit()
+    
+    uow = BatchUOW()
+    async with uow:
+        batch = await uow.batches.get({'reference': 'batch-1'})
+        line = OrderLine('o1', 'HIPSTER-WORKBENCH', 10)
+        batch.allocate(line)
+        await uow.commit()
+    
+    expected = await repo.get({'id': batch.id})
+    assert expected._allocations == {line}
+    assert expected.available_quantity == 90
