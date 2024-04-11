2a6f35b0a4484a51ef80822e610e4bdd4e19c4f7
Evgeny Kungurov <johnk2280@gmail.com>
2024-04-11 22:27:21 +0500

    [add] FakeUnitOfWork

---
+++ tests/fake_unit_of_work.py
@@ -0,0 +1,24 @@
+from typing import Any
+
+from domain import Batch
+from service_layer.ports import AbstractUOW
+from tests.fake_repository import FakeRepository
+
+
+class FakeUnitOfWork(AbstractUOW):
+    batches: FakeRepository[Batch]
+
+    def __init__(self) -> None:
+        self.is_committed = False
+
+    async def __aenter__(self) -> Any:
+        pass
+
+    async def __aexit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+        pass
+
+    async def commit(self) -> None:
+        self.is_committed = True
+
+    async def rollback(self) -> None:
+        pass
