d3314917519fa3064f38f4d7cc220ab458499bd3
Evgeny Kungurov <johnk2280@gmail.com>
2024-05-11 18:03:50 +0500

    [upd] test_api_returns_allocation

--- src/infrastructure/storage/repositories/__init__.py
+++ src/infrastructure/storage/repositories/__init__.py
@@ -1,7 +1,9 @@
 from .sqla_repository import AsyncBatchRepo
+from .sqla_repository import AsyncProductRepo
 from .sqla_repository import AsyncSQLARepository
 
 __all__ = [
     'AsyncSQLARepository',
     'AsyncBatchRepo',
+    'AsyncProductRepo',
 ]
