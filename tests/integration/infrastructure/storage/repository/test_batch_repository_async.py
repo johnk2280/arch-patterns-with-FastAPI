5d18c9597eaa615b4dc2fd4a2b810d1ff62f0946
Evgeny Kungurov <johnk2280@gmail.com>
2024-07-29 21:25:54 +0500

    [upd]

--- tests/integration/infrastructure/storage/repository/test_batch_repository_async.py
+++ tests/integration/infrastructure/storage/repository/test_batch_repository_async.py
@@ -50,6 +50,7 @@ async def test_async_batch_repository_can_save_a_batch_collection(
     ]
     product_repo = AsyncProductRepo(async_session)
     batch_repo = AsyncBatchRepo(async_session)
+
     await product_repo.add_many(
         [
             dict(sku='RUSTY-SOAPDISH'),
