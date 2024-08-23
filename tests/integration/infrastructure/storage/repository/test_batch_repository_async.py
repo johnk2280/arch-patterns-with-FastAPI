db639e0ac7692a8e3be2715e498de76ac23c60f4
Evgeny Kungurov <johnk2280@gmail.com>
2024-08-23 21:39:08 +0500

    [upd]

--- tests/integration/infrastructure/storage/repository/test_batch_repository_async.py
+++ tests/integration/infrastructure/storage/repository/test_batch_repository_async.py
@@ -69,7 +69,6 @@ async def test_async_batch_repository_can_save_a_batch_collection(
     assert len(rows) == 2
 
     assert res == rows
-
     assert rows == [Batch(**data) for data in batch_data]
 
 
