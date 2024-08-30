f659964591796cfd7c4c57318b9174f0d3da233e
Evgeny Kungurov <johnk2280@gmail.com>
2024-08-30 20:58:08 +0500

    [upd]

--- tests/e2e/api/test_allocate.py
+++ tests/e2e/api/test_allocate.py
@@ -137,7 +137,6 @@ async def test_400_message_for_out_of_stock(
     product_repo = AsyncProductRepo(async_session)
 
     await product_repo.add_many([dict(sku=sku)])
-
     await async_session.commit()
 
     await async_session.close()
