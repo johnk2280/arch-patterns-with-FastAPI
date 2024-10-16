46c9ed3c0aea46af6e1809883254d7cc6cd95335
Evgeny Kungurov <johnk2280@gmail.com>
2024-10-16 23:13:25 +0500

    [upd] jwt_validator()

---
+++ src/infrastructure/auth/jwt_checker.py
@@ -0,0 +1,15 @@
+from typing import Any
+
+from cryptography.x509 import load_pem_x509_certificate
+
+from config import get_settings
+
+settings = get_settings()
+
+
+def check_jwt(token: str) -> dict[str, Any]:
+    public_key_text = (
+            settings.BASE_DIR / 'src' / 'infrastructure' / 'auth'
+            / 'product_service_public_key.pem'
+    ).read_text()
+    public_key = load_pem_x509_certificate(public_key_text.encode())
